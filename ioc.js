887e789bd089cd5e41aa5d806c751951dcb99db9
Joakim J. Rapp <joakim.rapp@viaplay.com>
2014-01-16 16:49:11 +0100

    added multi register

--- ioc.js
+++ ioc.js
@@ -1,42 +1,42 @@
 ( function() {
-	var path = require( 'path' ), fs = require( 'fs' ),
+	var pub = {},
+		path = require( 'path' ), fs = require( 'fs' ),
 		log = require( './log.js' )( 'ioc' ),
 		settings = require( './settings.js' )( require( './log.js' )( 'settings' ) ),
 		container = require( './container.js' )( require( './log.js' )( 'container' ) ),
 		files = require( './files.js' )( path, fs, require( './log.js' )( 'files' ), require( 'path' ).dirname( module.parent.filename ) ),
 		startedCallback,
-		started = false,
-
-	register = function( name, pathOrLoaded, lifecycleTransient ) {
+		started = false;
+	pub.register = function( name, pathOrLoaded, lifecycleTransient ) {
 		if( typeof( pathOrLoaded ) == 'string' )
 			container.register( name, require( files.getFullPath( pathOrLoaded ) ), lifecycleTransient ? false : true );
 		else
 			container.load( name, pathOrLoaded );
-		return ioc;
-	},
-	registerRequired = function( name, required, lifecycleTransient ) {
+		return pub;
+	};
+	pub.registerRequired = function( name, required, lifecycleTransient ) {
 		container.register( name, required, lifecycleTransient ? false : true );
-		return ioc;
-	},
-	registerDependency = function( name, loaded ) {
+		return pub;
+	};
+	pub.registerDependency = function( name, loaded ) {
 		container.registerDependency( name, loaded );
-		return ioc;
-	},
-	registerLib = function( required ) {
+		return pub;
+	};
+	pub.registerLib = function( required ) {
 		container.register( undefined, required, true );
-		return ioc;
-	},
-	autoRegister = function( relativePath ) {
+		return pub;
+	};
+	pub.autoRegister = function( relativePath ) {
 		log.trace( 'ioc', 'Auto regestering', relativePath, undefined );
-		files.findValidFiles( relativePath, register );
-		return ioc;
-	},
-	wrap = function( name, wrapperName ) {
+		files.findValidFiles( relativePath, pub.register );
+		return pub;
+	};
+	pub.wrap = function( name, wrapperName ) {
 		log.info( 'Wrapping', wrapperName + '( ' + name + ' )' );
 		container.wrap( name, wrapperName );
-		return ioc;
-	},
-	start = function( callback ) {
+		return pub;
+	};
+	pub.start = function( callback ) {
 		log.info( 'ioc', 'Starting by resolving all', undefined, undefined );
 		container.resolveAll( function() {
 			log.info( 'ioc', 'All resolved', undefined, undefined );
@@ -46,38 +46,38 @@
 			if( startedCallback )
 				container.inject( startedCallback );
 		} );
-		return ioc;
-	},
-	inject = function( fn ) {
+		return pub;
+	};
+	pub.inject = function( fn ) {
 		container.inject( fn );
-		return ioc;
-	},
-	reset = function() {
+		return pub;
+	};
+	pub.reset = function() {
 		container.reset();
 		settings.reset();
-		register( 'ioc', ioc );
-		return ioc;
-	},
-	setLogLevel = function( level ) {
+		pub.register( 'ioc', pub );
+		return pub;
+	};
+	pub.setLogLevel = function( level ) {
 		log.setLogLevel( level );
-		return ioc;
-	},
-	setStartedCallback = function( fn ) {
+		return pub;
+	};
+	pub.setStartedCallback = function( fn ) {
 		startedCallback = fn;
 		if( started )
-			inject( startedCallback );
-		return ioc;
-	},
-	setSettings = function( name, data ) {
+			pub.inject( startedCallback );
+		return pub;
+	};
+	pub.setSettings = function( name, data ) {
 		settings.set( data );
-		register( name, data );
-		return ioc;
-	},
-	conditionalAutoRegister = function( settingsKey, conditionalValue, path ) {
+		pub.register( name, data );
+		return pub;
+	};
+	pub.conditionalAutoRegister = function( settingsKey, conditionalValue, path ) {
 		log.trace( 'ioc', 'ConditionalAutoRegister', settingsKey, undefined );
-		return ( settings.matchesSetting( settingsKey, conditionalValue ) ) ? autoRegister( path ) : ioc;
-	},
-	conditionalPathAutoRegister = function( settingsKey, basePath ) {
+		return ( settings.matchesSetting( settingsKey, conditionalValue ) ) ? pub.autoRegister( path ) : pub;
+	};
+	pub.conditionalPathAutoRegister = function( settingsKey, basePath ) {
 		log.trace( 'ioc', 'ConditionalPathAutoRegister', settingsKey, undefined );
 		var settingsValue = settings.getSetting( settingsKey );
 		if( settingsValue ) {
@@ -85,61 +85,48 @@
 				basePath += '/';
 			return autoRegister( basePath + settingsValue );
 		}
-		else return ioc;
-	},
-	conditionalRegister = function( settingsKey, conditionalValue, name, pathOrLoaded, lifecycleTransient ) {
+		else return pub;
+	};
+	pub.conditionalRegister = function( settingsKey, conditionalValue, name, pathOrLoaded, lifecycleTransient ) {
 		log.trace( 'ioc', 'ConditionalRegister', settingsKey, undefined );
-		return ( settings.matchesSetting( settingsKey, conditionalValue ) ) ? register( name, pathOrLoaded, lifecycleTransient ) : ioc;
-	},
-	conditionalRegisterRequired = function( settingsKey, conditionalValue, name, required, lifecycleTransient ) {
+		return ( settings.matchesSetting( settingsKey, conditionalValue ) ) ? pub.register( name, pathOrLoaded, lifecycleTransient ) : pub;
+	};
+	pub.conditionalRegisterRequired = function( settingsKey, conditionalValue, name, required, lifecycleTransient ) {
 		log.trace( 'ioc', 'ConditionalRegisterRequired', settingsKey, undefined );
-		return ( settings.matchesSetting( settingsKey, conditionalValue ) ) ? registerRequired( name, required, lifecycleTransient ) : ioc;
-	},
-	wrapFromSettings = function( settingsKey ) {
+		return ( settings.matchesSetting( settingsKey, conditionalValue ) ) ? pub.registerRequired( name, required, lifecycleTransient ) : pub;
+	};
+	pub.wrapFromSettings = function( settingsKey ) {
 		log.trace( 'wrapFromSettings', settingsKey );
 		var wrapperSettings = settings.getSetting( settingsKey );
 		if( wrapperSettings ) {
 			for( var name in wrapperSettings )
-				wrap( name, wrapperSettings[ name ] );
+				pub.wrap( name, wrapperSettings[ name ] );
 		}
-		return ioc;
-	},
-
-	setWaitingWarningTime = function( milliseconds ) {
+		return pub;
+	};
+	pub.setWaitingWarningTime = function( milliseconds ) {
 		container.setWaitingWarningTime( milliseconds );
-		return ioc;
-	},
-	setLogger = function( name, logger ) {
+		return pub;
+	};
+	pub.setLogger = function( name, logger ) {
 		var resolvedLogger = logger( 'ioc', settings.getSettings() );
-		registerRequired( name, logger, true );
+		pub.registerRequired( name, logger, true );
 		log = resolvedLogger;
 		settings.setLogger( logger( 'settings', settings.getSettings() ) );
 		container.setLogger( logger( 'container', settings.getSettings() ) );
 		files.setLogger( logger( 'files', settings.getSettings() ) );
-		return ioc;
-	},
-	ioc = {
-		setLogLevel: setLogLevel,
-		setLogger: setLogger,
-		register: register,
-		registerRequired: registerRequired,
-		autoRegister: autoRegister,
-		start: start,
-		inject: inject,
-		reset: reset,
-		setStartedCallback: setStartedCallback,
-		setSettings: setSettings,
-		conditionalAutoRegister: conditionalAutoRegister,
-		conditionalPathAutoRegister: conditionalPathAutoRegister,
-		conditionalRegister: conditionalRegister,
-		conditionalRegisterRequired: conditionalRegisterRequired,
-		setWaitingWarningTime: setWaitingWarningTime,
-		wrap: wrap,
-		wrapFromSettings: wrapFromSettings,
-		registerLib: registerLib,
-		registerDependency: registerDependency
+		return pub;
 	};
-
-	register( 'ioc', ioc );
-	module.exports = ioc;
+	pub.multiRegisterSingletons = function( compnents ) {
+		for( var name in components )
+			pub.register( name, components[ name ] );
+		return pub;
+	};
+	pub.multiRegisterLibs = function( libs ) {
+		libs.forEach( function( lib ) {
+			pub.registerLib( lib );
+		} );
+	};
+	pub.register( 'ioc', pub );
+	module.exports = pub;
 } ).call( this );
